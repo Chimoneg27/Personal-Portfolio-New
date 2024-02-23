902040fa711146f4492475f8d2ca5d08b20216bf
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-02-23 20:20:07 +0200

    add skeleton for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react';
 import '../App.css';
 
 const Navbar = () => {
-  const [color, setColor] = useState('white');
 
-  <>
-    <nav className='w-80 p-3' style={{backgroundColor: {color}}}>
-    <button onClick={setColor('black')}>black</button>
-    <button onClick={setColor('white')}>white</button>
-    </nav>
-  </>
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
 }
 
 export default Navbar;
