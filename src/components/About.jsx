ddea7f394b37a0e0fc33a68dd0e37279f8599009
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-02-28 16:43:27 +0200

    Start about section

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div className='py-20'>
+      <div>
+        <h2>ABOUT ME</h2>
+
+        <p>
+            Here you will find more information about and what I do in terms of programming, my current skills and some of my hobbies.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About;
