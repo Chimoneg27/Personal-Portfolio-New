9ad1bff0088fddd3fea94e82a82b160705767a71
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-03-01 21:57:20 +0200

    start about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,15 @@
 const About = () => {
   return (
     <div className='py-20'>
-      <div>
+      <div className="about-top">
         <h2>ABOUT ME</h2>
 
         <p>
             Here you will find more information about and what I do in terms of programming, my current skills and some of my hobbies.
         </p>
+      </div>
+      <div className='me-plus-tech'>
+        <h2>Garvin Chimone</h2>
       </div>
     </div>
   )
