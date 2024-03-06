834028abc486402e7c54c4e0ca9baa3e2da076ae
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-03-06 22:18:30 +0200

    add about layout skeleton

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,8 +8,13 @@ const About = () => {
             Here you will find more information about and what I do in terms of programming, my current skills and some of my hobbies.
         </p>
       </div>
-      <div className='me-plus-tech'>
+      <div className='tech-stack'>
         <h2>Garvin Chimone</h2>
+      </div>
+      <div className='about'>
+        <h2>
+          About me
+        </h2>
       </div>
     </div>
   )
