5c0d6b9f0538acb9ed5f28e9cc9f16fe36386ec2
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-03-19 16:10:27 +0200

    add mobile responsiveness for banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -39,20 +39,20 @@ const Banner = () => {
         backgroundRepeat: "no-repeat"
       }}
     >
-      <div className="short-intro p-2 w-4/5 mt-20  flex flex-col items-center ">
+      <div className="short-intro p-2 mt-20  flex flex-col items-center">
         <h1 className="text-6xl font-bold text-center text-blue-700" ref={glitch.ref}>
           HELLO, I&apos;M GARVIN CHIMONE
         </h1>
         <p className="text-xl text-center mt-10 font-semibold text-black">
           I am a full-stack web developer on a misson to deliver exceptional web applications.
         </p>
-        <p className="text-xl text-center font-semibold text-black">
+        <p className="text-xl text-center font-semibold text-black positi">
           Let&apos;s make something special.
         </p>
 
         <button
           type="button"
-          className="px-4 py-3 mt-24 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-y-75 transition-transform flex w-80 items-center"
+          className="project-btn px-4 py-3 mt-24 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-y-75 transition-transform flex w-80 items-center"
         >
           <span className="ml-2 w-full text-xl font-bold">PROJECTS</span>
         </button>
